f17e604cf94c4e6a0b4278531dcece0885714ea9
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-06-03 18:07:33 +0200

    Update mainwindow.cpp

--- Rock_Paper_Scissors/mainwindow.cpp
+++ Rock_Paper_Scissors/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include <QPushButton>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -28,7 +29,7 @@ void MainWindow::setLayoutForTitleScreen()
 
     //set layout with quit button
     quitButton = new QPushButton("QUIT");
-    quitButton ->setMinimumSize(80,30);
+    quitButton -> setMinimumSize(80,30);
 
     hQuitSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
@@ -38,6 +39,7 @@ void MainWindow::setLayoutForTitleScreen()
 
     //set layout with rounds editor
     roundsLabel = new QLabel("ROUNDS YOU WANT TO PLAY: ");
+    roundsLabel -> wordWrap();
     roundsLabel -> setStyleSheet("font-size:20px;");
     roundsLabel -> setFrameStyle(QFrame::Panel);
     roundsLabel -> setLineWidth(1);
@@ -112,9 +114,9 @@ void MainWindow::setLayoutForTitleScreen()
     hBetweenRulesAndBeginSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
     mainBodyLayout = new QHBoxLayout();
-    mainBodyLayout -> insertLayout(0, rulesLayout);
+    mainBodyLayout -> insertLayout(0, rulesLayout, 2);
     mainBodyLayout -> insertSpacerItem(1, hBetweenRulesAndBeginSpacer);
-    mainBodyLayout -> insertLayout(2, roundsAndBeginLayout);
+    mainBodyLayout -> insertLayout(2, roundsAndBeginLayout, 1);
 
     //set central widget's layout
     mainLayout = new QVBoxLayout(ui->centralwidget);
@@ -130,5 +132,71 @@ void MainWindow::setLayoutForTitleScreen()
 
 void MainWindow::beginGame()
 {
-    qDebug() << "Button was clicked";
+    saveEditedInput();
+    removeLayoutForTitleScreen();
+}
+
+void MainWindow::saveEditedInput()
+{
+    //save input from text edit into std::string
+    std::string lineInput = roundsEdit->text().toStdString();
+    int length = lineInput.length();
+    bool isItValid = true, ok;
+
+    for (int i=0; i<length; i++)
+    {
+        if (lineInput[i] < 48 || lineInput[i] > 57)
+        {
+            isItValid = false;
+            break;
+        }
+    }
+
+    if (lineInput[0] == '0' || lineInput == "")
+        isItValid = false;
+
+    if (isItValid == true && roundsEdit->text().toInt(&ok, 10) <= 100)
+        numberOfRounds = roundsEdit->text().toInt(&ok, 10);
+    else if (isItValid == true && roundsEdit->text().toInt(&ok, 10) > 100)
+        QMessageBox::warning(roundsEdit, "Too big input", "Don't you think your input is... too big? You don't want to spend too much time"
+                                                          " here!");
+    else
+        QMessageBox::critical(roundsEdit, "Invalid input", "Number of rounds you want to play is incorrect");
+}
+
+void MainWindow::removeLayoutForTitleScreen()
+{
+    //delete buttons
+    delete quitButton;
+    delete beginButton;
+    //delete labels
+    delete titleLabel;
+    delete rulesLabel;
+    delete roundsLabel;
+
+    delete rBeatsSLabel_Rock;
+    delete rBeatsSLabel_Beats;
+    delete rBeatsSLabel_Scissors;
+
+    delete sBeatsPLabel_Scissors;
+    delete sBeatsPLabel_Beats;
+    delete sBeatsPLabel_Paper;
+
+    delete pBeatsRLabel_Paper;
+    delete pBeatsRLabel_Beats;
+    delete pBeatsRLabel_Rock;
+    //delete edit
+    delete roundsEdit;
+    //delete layouts
+    delete quitLayout;
+
+    delete rBsLayout;
+    delete sBpLayout;
+    delete pBrLayout;
+
+    delete rulesLayout;
+
+    delete roundsLayout;
+    delete roundsAndBeginLayout;
+    delete mainBodyLayout;
 }
