430c3293cd616445ad63e34d1ae78b432b7db691
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-06-05 00:00:19 +0200

    Update mainwindow.h
    Add objects for game screen

--- Rock_Paper_Scissors/mainwindow.h
+++ Rock_Paper_Scissors/mainwindow.h
@@ -6,6 +6,7 @@
 #include <QLineEdit>
 #include <QLabel>
 #include <QSpacerItem>
+#include <QLCDNumber>
 #include <QLayout>
 
 QT_BEGIN_NAMESPACE
@@ -20,16 +21,22 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private slots:
+    //TITLE SCREEN FUNCTIONS
     void setLayoutForTitleScreen();
     void setStylesForTitleScreen();
 
     void beginGame();
     bool saveEditedInput();
     void removeLayoutForTitleScreen();
-    //void setLayoutForGameScreen();
+
+    //GAME SCREEN FUNCTIONS
+    void setLayoutForGameScreen();
 
 private:
     Ui::MainWindow *ui;
+    int numberOfRounds;
+
+    //TITLE SCREEN
     QPushButton *quitButton, *beginButton;
     QLabel *titleLabel, *rulesLabel, *roundsLabel, *rBeatsSLabel_Rock, *rBeatsSLabel_Beats, *rBeatsSLabel_Scissors,
             *sBeatsPLabel_Scissors, *sBeatsPLabel_Beats, *sBeatsPLabel_Paper, *pBeatsRLabel_Paper, *pBeatsRLabel_Beats,
@@ -39,7 +46,12 @@ private:
     QVBoxLayout *mainLayout, *rulesLayout, *roundsAndBeginLayout;
     QHBoxLayout *quitLayout, *mainBodyLayout, *roundsLayout, *rBsLayout, *sBpLayout, *pBrLayout, *beginButtonLayout;
 
-    int numberOfRounds;
-
+    //GAME SCREEN
+    QVBoxLayout *pointsMainLayout, *userPointsLayout, *compPointsLayout, *gameInterfaceLayout;
+    QHBoxLayout *iconsLayout, *gameBodyLayout;
+    QLabel *userScoreLabel, *compScoreLabel, *rockLabel, *paperLabel, *scissorsLabel, *userLabel, *compLabel, *userChoiceLabel,
+            *compChoiceLabel, *loadingLabel;
+    QLCDNumber *userScoreLcd, *compScoreLcd;
+    QSpacerItem *vBetweenLcdSpacer, *hLeftSpacer, *hRightSpacer;
 };
 #endif // MAINWINDOW_H
