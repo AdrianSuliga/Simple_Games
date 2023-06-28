c15b265d9afbaf9b41dae568f24360a9f65717f4
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-06-28 22:36:35 +0200

    Update mainwindow.h
    Add title screen layout

--- Amethyst_Clicker/mainwindow.h
+++ Amethyst_Clicker/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+#include <QSpacerItem>
+#include <QBoxLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,8 +18,22 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private slots:
+    void setLayoutTitleScreen();
+    void setStyleTitleScreen();
 
 private:
     Ui::MainWindow *ui;
+    //GAME VARIABLES
+    double points, multiplier;
+    int hammers, pickaxes, children, drills, dynamite;
+    //TITLE SCREEN
+    QPushButton *continueButton, *newgameButton, *tutorialButton, *aboutButton, *quitButton;
+    QLabel *titleLabel;
+    QSpacerItem *lcSpacer, *rcSpacer, *lnSpacer, *rnSpacer, *ltSpacer, *rtSpacer, *laSpacer, *raSpacer, *lqSpacer, *rqSpacer;
+    QVBoxLayout *mainLayout;
+    QHBoxLayout *continueLayout, *newgameLayout, *tutorialLayout, *aboutLayout, *quitLayout;
+    //CHOOSE SAVE SCREEN
+
 };
 #endif // MAINWINDOW_H
