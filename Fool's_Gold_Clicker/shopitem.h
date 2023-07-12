940e99f54e1b6dcadaaff3481f4be1d86b850c4a
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-07-12 15:52:06 +0200

    Update shopitem.h

--- Fool's_Gold_Clicker/shopitem.h
+++ Fool's_Gold_Clicker/shopitem.h
@@ -10,18 +10,19 @@ class ShopItem : public QWidget
 {
     Q_OBJECT
 public:
-    explicit ShopItem(QWidget* parent = nullptr, QString path = "", int price = 0);
+    explicit ShopItem(QWidget* parent = nullptr, QString path = "", double price = 0.0);
     ~ShopItem();
+    QLabel *priceLabel;
 signals:
     void clicked();
 protected:
     void mousePressEvent(QMouseEvent *event);
     void paintEvent(QPaintEvent *event);
 private slots:
-    void setLayoutItemShop(QString path, int price);
+    void setLayoutItemShop(QString path, double price);
     void setStyleItemShop();
 private:
-    QLabel *iconLabel, *priceLabel;
+    QLabel *iconLabel;
     QVBoxLayout *mlayout;
     QFont Bohemian;
 };
