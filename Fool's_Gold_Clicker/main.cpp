6f940fc8e79e979914d68fcab141333ab9d49d61
AdrianSuliga <128411632+AdrianSuliga@users.noreply.github.com>
2023-07-20 01:08:32 +0200

    Update main.cpp

--- Fool's_Gold_Clicker/main.cpp
+++ Fool's_Gold_Clicker/main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCoreApplication::setOrganizationName("NULL");
+    QCoreApplication::setApplicationName("FOOL'S GOLD");
     MainWindow w;
     w.show();
     return a.exec();
